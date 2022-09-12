141ecad6b21b5226ccf4e3431cf0f4b3a8071e61
deepak852k1 <deepak852k1@gmail.com>
2022-09-12 13:22:03 +0530

    pages created

---
+++ src/pages/AddEdit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddEdit() {
+  return (
+    <div><h1> ADD EDIT</h1></div>
+  )
+}
+
+export default AddEdit
---
+++ src/pages/View.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function View() {
+  return (
+    <div><h1> View</h1></div>
+  )
+}
+
+export default View
